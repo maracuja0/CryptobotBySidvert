757561a5d283bd6d9c54ff4b618535838b08f5f0
Maracuja0 <sorokinjr15@yandex.ru>
2022-08-28 22:27:35 +0400

    Доработан header

--- index.php
+++ index.php
@@ -14,31 +14,39 @@
 </head>
 
 <body>
-    <header class="p-3 text-bg-dark">
+    <header class="p-3">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                <a href="/" class="d-flex align-items-center mt-2 mb-2 mb-lg-0 text-white text-decoration-none">
+                <img width="50" height ="40" src="img/logo.png">
                 </a>
-
+                
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Главная</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Бот</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Сделки</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Отчетность</a></li>
                 </ul>
 
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-                </form>
-
                 <div class="text-end">
-                <button type="button" class="btn btn-outline-light me-2">Login</button>
-                <button type="button" class="btn btn-warning">Sign-up</button>
+                
+                    <div class="row">
+                        <div class="col-sm">
+                            <form action="scripts/auth.php" method="POST">
+                                <button id="auth_button"name = "auth" type="submit" class="btn btn-outline-light me-2">Войти</button>
+                            </form>
+                        </div>
+
+                        <div class="col-sm">
+                            <form action="scripts/reg.php" method="POST">
+                                <button id="reg_button" name = "reg "type="submit" class="btn btn-warning">Зарегистрироваться</button>
+                            </form>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-      </div>
-    </div>
+        </div>
   </header>
 
     <!--<script src="js/script.js"></script>-->
